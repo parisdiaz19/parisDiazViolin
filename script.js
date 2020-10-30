15f6615b0eae4d87d334029e8e8fee5efd7b3532
Paris Diaz <68017227+parisdiaz19@users.noreply.github.com>
2020-10-30 14:27:22 -0400

    l

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const button = document.getElementById('submit');
+const firstName = document.getElementById('first_name').value;
+const lastName = document.getElementById('last_name').value;
+const email = document.getElementById('email').value;
+const phoneNumber = document.getElementById('number').value;
 
 const contact = (e) => {
     button.addEventListener('click', (e) => {
         e.preventDefault();
-        alert('Form has been submitted. Thank you!')
+        if (firstName === "" && 
+            lastName === "" &&
+            email === "" &&
+            phoneNumber === "") {
+            alert('Please fill in the sign in form to submit.');
+        } else {
+            alert('Form has been submitted. Thank you!');
+        }
+        
     });
 
 };
